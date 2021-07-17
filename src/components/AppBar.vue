85a4746bf01d2081ad6145d2279438193a9780bf
Vinicius Pereira de Oliveira <deoliveiradev@gmail.com>
2021-07-17 14:48:33 -0300

    AppBar add

---
+++ src/components/AppBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card>
+    <!-- BARRA DE MENU -->
+    <v-app-bar color="dark" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Conectamos!</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>ONG's</v-tab>
+          <v-tab>EMPRESAS</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+
+    <!-- SIDEBAR CONFIG -->
+    <v-navigation-drawer v-model="drawer" height="100vh" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="dark">
+          <!-- SIDEBAR ITEM 1 -->
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <!-- SIDEBAR ITEM 2 -->
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/EmpresasArea.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card max-width="400" class="mx-auto">
+    <v-container>
+      <v-row dense>
+        <v-col v-for="(empresas, i) in empresas" :key="i" cols="12">
+          <v-card color="grey darken-1" dark>
+            <div>
+              <div class="d-flex justify-center">
+
+                <v-avatar class="mt-3 mb-3" height="auto" width="200" tile>
+                  <v-img :src="empresas.logo"></v-img>
+                </v-avatar>
+
+              </div>
+
+                <v-card-actions>
+                  <v-btn block>
+                    Mais Informações
+                  </v-btn>
+                </v-card-actions>
+
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "EmpresasArea",
+  data() {
+    return {
+      empresas: [],
+    };
+  },
+  created() {
+    fetch("https://it3-gdf-default-rtdb.firebaseio.com/alimentos/empresas.json")
+      .then((resposta) => resposta.json())
+      .then((json) => {
+        this.empresas = json;
+        console.log(this.empresas);
+      });
+  },
+};
+</script>
+
+<style scoped>
+</style>
