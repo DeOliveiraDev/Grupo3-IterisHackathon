8f3f5ec3a6d8ac0674ac79ac005e7f85c4b8e5bf
cosmiana barbosa <cosmianabarbosa@gmail.com>
2021-07-18 15:37:30 -0300

    atualizando

--- src/views/PaginaInicial.vue
+++ src/views/PaginaInicial.vue
@@ -42,31 +42,33 @@
             <div class="Nossa Historia">
                 <h1> Nossa historia</h1>
 
-               <p>Desde o início da pandemia, em 2020, estamos passando por um momento muito difícil.</p>
-               <p>Acreditamos que as dificuldades podem ser amenizadas com solidariedade, união, trabalho, 
-                boa dose de coragem, aliada a uma gestão competente.</p>
-                <p>Mesmo com as dificuldades impostas pela crise muitos setores tem se renovado com 
-                novas formas de realizar suas atividades, além de surgirem novas parcerias.</p> 
-                <p>Dentre esses setores temos empresas,Ong's e profissionais da saúde, arte cultura e outras áreas.</p>
                 
-                <p>Para contribuir melhor desenvolvimento desse cenário surge a Connect, que visa conctar artista,
+               <p class="text-justify pa-md-10 mx-lg-auto">Desde o início da pandemia, em 2020, estamos passando por um momento muito difícil.
+                Acreditamos que as dificuldades podem ser amenizadas com solidariedade, união, trabalho, 
+                boa dose de coragem, aliada a uma gestão competente.
+                Mesmo com as dificuldades impostas pela crise muitos setores tem se renovado com 
+                novas formas de realizar suas atividades, além de surgirem novas parcerias.
+                Dentre esses setores temos empresas,Ong's e profissionais da saúde, arte cultura e outras áreas.                
+                Para contribuir melhor desenvolvimento desse cenário surge a Connect, que visa conctar artista,
                  profissionais de varias áreas, ong's e empresas.</p>
+
                     <!-- Missão,Visão e valores -->
 
                 
                 <h2> Missão</h2>
-                <p>
-                 Realizando conexões, queremos combater a fome, o desemprego e outros problemas potencializados pela pandemia.
+                
+                 <p class="linha">Realizando conexões, queremos combater a fome, o desemprego e outros problemas potencializados pela pandemia.
                 </p>
 
                 <h2> Visão</h2>
-                <p>“Consolidar a nossa atuação como entidade de referência no nosso segmento, 
+                
+                <p class="linha">“Consolidar a nossa atuação como entidade de referência no nosso segmento, 
                 sendo também reconhecida em âmbito internacional, tornando-se autossuficiente com a 
                 participação da sociedade organizada”.</p>
             
                 
                 <h2> Valores</h2>
-                <p>Atender as pessoas, profissionais, empresas e ong's com qualidade, eficácia e eficiência de forma a promover 
+                <p class="linha">Atender as pessoas, profissionais, empresas e ong's com qualidade, eficácia e eficiência de forma a promover 
                   seus serviços e ações, minimizando vulnerabilidades sociais.</p>
             
 
@@ -74,7 +76,7 @@
                 
                 <h2>O que nossa empresa oferece</h2>
 
-                <p>Oferecemos uma plataforma que facilita a conexão entre os setores ja citados, poupando tempo e recursos.</p>
+                <p class="linha">Oferecemos uma plataforma que facilita a conexão entre os setores ja citados, poupando tempo e recursos.</p>
                 
            </div>
 
